dc72476a6029f3ec502fac9a6bcb6096fc1c4a6e
Seongbin Lim <seongbin.lim@polytechnique.edu>
2022-02-07 16:50:44 +0100

    Expose main funcs classes on top module

--- bioimageloader/__init__.py
+++ bioimageloader/__init__.py
@@ -1,6 +1,9 @@
+from .batch import BatchDataloader, ConcatDataset
 from .config import Config, datasets_from_cfg
 
 __all__ = [
+    'BatchDataloader',
+    'ConcatDataset',
     'Config',
-    'datasets_from_cfg'
+    'datasets_from_cfg',
 ]
