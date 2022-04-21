13296fbc4c2b73f4ff415c9ce260ccac71a30357
xjzhaang <xingjian.zhang@polytechnique.edu>
2022-04-21 10:43:23 +0200

    1. sorted the files based on int value 2. deleted grayscale 3. image transformed to 3 channel 4. took blue channel as mask

--- bioimageloader/collections/_bbbc004.py
+++ bioimageloader/collections/_bbbc004.py
@@ -1,6 +1,6 @@
 from functools import cached_property
-from pathlib import Path
-from typing import List, Optional, Sequence, Union
+from pathlib import Path, PurePath
+from typing import List, Optional, Sequence, Union, Dict
 
 import albumentations
 import cv2
@@ -12,19 +12,19 @@ from ..base import MaskDataset
 
 class BBBC004(MaskDataset):
     """Synthetic cells
-    
+
     Biological application
 
     One of the principal challenges in counting or segmenting nuclei is dealing with clustered nuclei.
-    To help assess algorithms' performance in this regard, this synthetic image set consists of five 
+    To help assess algorithms' performance in this regard, this synthetic image set consists of five
     subsets with increasing degree of clustering.
-    
+
     Images
 
-    Five subsets of 20 images each are provided. Each image contains 300 objects, but the objects overlap 
-    and cluster with different probabilities in the five subsets. The images were generated with the SIMCEP 
-    simulating platform for fluorescent cell population images (Lehmussola et al., IEEE T. Med. Imaging, 2007 and Lehmussola et al., P. IEEE, 2008). 
-    
+    Five subsets of 20 images each are provided. Each image contains 300 objects, but the objects overlap
+    and cluster with different probabilities in the five subsets. The images were generated with the SIMCEP
+    simulating platform for fluorescent cell population images (Lehmussola et al., IEEE T. Med. Imaging, 2007 and Lehmussola et al., P. IEEE, 2008).
+
     Parameters
     ----------
     root_dir : str
@@ -37,18 +37,11 @@ class BBBC004(MaskDataset):
     num_samples : int, optional
         Useful when ``transforms`` is set. Define the total length of the
         dataset. If it is set, it overwrites ``__len__``.
-    grayscale : bool, default: False
-        Convert images to grayscale
-    grayscale_mode : {'cv2', 'equal', Sequence[float]}, default: 'cv2'
-        How to convert to grayscale. If set to 'cv2', it follows opencv
-        implementation. Else if set to 'equal', it sums up values along channel
-        axis, then divides it by the number of expected channels.
-        
     References
     ----------
     .. [1] https://bbbc.broadinstitute.org/BBBC004
-    
-    
+
+
     See Also
     --------
     MaskDataset : Super class
@@ -75,13 +68,12 @@ class BBBC004(MaskDataset):
 
     def get_image(self, p: Path) -> np.ndarray:
         img = tifffile.imread(p)
-        #img = cv2.cvtColor(img, cv2.GRAYSCALE)
+        img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
         return img
 
     def get_mask(self, p: Path) -> np.ndarray:
         mask = tifffile.imread(p)
-        mask = cv2.cvtColor(mask, cv2.COLOR_RGB2GRAY)
-        th, mask = cv2.threshold(mask, 0, 255, cv2.THRESH_BINARY)
+        mask = mask[:, :, 2]
         return mask
 
     @cached_property
@@ -89,13 +81,14 @@ class BBBC004(MaskDataset):
         # Important to decorate with `cached_property` in general
         root_dir = self.root_dir
         parent = '*_images'
-        file_list = sorted(root_dir.glob(f'{parent}/*.tif'))
+        file_list = sorted(root_dir.glob(f'{parent}/*.tif'), key=lambda x: int(x.stem.split("GRAY")[0]))
         return file_list
 
     @cached_property
-    def anno_dict(self) -> List[Path]:
+    def anno_dict(self) -> Dict[int, Path]:
         # Important to decorate with `cached_property` in general
         root_dir = self.root_dir
         parent = '*_foreground'
-        file_list = sorted(root_dir.glob(f'{parent}/*.tif'))
-        return file_list
+        anno_list = sorted(root_dir.glob(f'{parent}/*.tif'), key=lambda x: int(x.stem))
+        anno_dict = dict((k, v) for k, v in enumerate(anno_list))
+        return anno_dict
