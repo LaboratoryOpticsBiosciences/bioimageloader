0517a37d612bd226bb1ffc894ea11a974ee3817a
xjzhaang <xingjian.zhang@polytechnique.edu>
2022-04-21 10:08:32 +0200

    1. changed anno_dict to return dictionary 2. removed grayscale feature

--- bioimageloader/collections/_bbbc030.py
+++ bioimageloader/collections/_bbbc030.py
@@ -1,6 +1,6 @@
 from functools import cached_property
 from pathlib import Path
-from typing import List, Optional, Sequence, Union
+from typing import List, Optional, Sequence, Union, Dict
 
 import albumentations
 import numpy as np
@@ -11,10 +11,10 @@ from ..base import MaskDataset
 
 class BBBC030(MaskDataset):
     """Chinese Hamster Ovary Cells
-    
-    The image set consists of 60 Differential Interference Contrast (DIC) images of Chinese Hamster Ovary (CHO) cells. 
-    The images are taken on an Olympus Cell-R microscope with a 20x lens at the time when the cell initiated their attachment to the bottom of the dish. 
-    
+
+    The image set consists of 60 Differential Interference Contrast (DIC) images of Chinese Hamster Ovary (CHO) cells.
+    The images are taken on an Olympus Cell-R microscope with a 20x lens at the time when the cell initiated their attachment to the bottom of the dish.
+
     Parameters
     ----------
     root_dir : str
@@ -27,21 +27,15 @@ class BBBC030(MaskDataset):
     num_samples : int, optional
         Useful when ``transforms`` is set. Define the total length of the
         dataset. If it is set, it overwrites ``__len__``.
-    grayscale : bool, default: False
-        Convert images to grayscale
-    grayscale_mode : {'cv2', 'equal', Sequence[float]}, default: 'cv2'
-        How to convert to grayscale. If set to 'cv2', it follows opencv
-        implementation. Else if set to 'equal', it sums up values along channel
-        axis, then divides it by the number of expected channels.
     See Also
     --------
     MaskDataset : Super class
     DatasetInterface : Interface
-    
+
     References
     ----------
     .. [1] https://bbbc.broadinstitute.org/BBBC030
-    
+
     """
     # Set acronym
     acronym = 'BBBC030'
@@ -53,8 +47,6 @@ class BBBC030(MaskDataset):
         output: str = 'both',
         transforms: Optional[albumentations.Compose] = None,
         num_samples: Optional[int] = None,
-        grayscale: bool = False,  # optional
-        grayscale_mode: Union[str, Sequence[float]] = 'cv2',  # optional
         # specific to this dataset
         **kwargs
     ):
@@ -62,8 +54,6 @@ class BBBC030(MaskDataset):
         self._output = output
         self._transforms = transforms
         self._num_samples = num_samples
-        self._grayscale = grayscale   # optional
-        self._grayscale_mode = grayscale_mode  # optional
         # specific to this one here
 
     def get_image(self, p: Path) -> np.ndarray:
@@ -84,9 +74,10 @@ class BBBC030(MaskDataset):
         return file_list
 
     @cached_property
-    def anno_dict(self) -> List[Path]:
+    def anno_dict(self) -> Dict[int, Path]:
         # Important to decorate with `cached_property` in general
         root_dir = self.root_dir
         parent = 'ground_truth'
-        file_list = sorted(root_dir.glob(f'{parent}/*.png'))
-        return file_list
+        anno_list = sorted(root_dir.glob(f'{parent}/*.png'))
+        anno_dict = dict((k, v) for k, v in enumerate(anno_list))
+        return anno_dict
