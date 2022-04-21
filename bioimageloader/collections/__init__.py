7c2dcfdf8064884a8cf698f554ef1f5e63f70d69
xjzhaang <56111819+xjzhaang@users.noreply.github.com>
2022-04-21 12:08:14 +0200

    pull request bbbc009 (#2)
    * add bbbc009
    
    * 1. added description to bbbc009
    2. anno_dict to dictionary
    
    Co-authored-by: Seongbin Lim <58146755+sbinnee@users.noreply.github.com>

--- bioimageloader/collections/__init__.py
+++ bioimageloader/collections/__init__.py
@@ -14,6 +14,7 @@ from ._bbbc006 import BBBC006
 from ._bbbc020 import BBBC020
 from ._bbbc039 import BBBC039
 from ._cellpose import Cellpose
+from ._bbbc009 import BBBC009
 from ._bbbc030 import BBBC030
 #     semantic (fg/bg)
 from ._tnbc import TNBC
@@ -44,6 +45,7 @@ __all__ = [
     'BBBC006',
     'BBBC007',
     'BBBC008',
+    'BBBC009',
     'BBBC013',
     'BBBC014',
     'BBBC015',
